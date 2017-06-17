069e8548df3a11bf96a59bccf042e896531eb3ab
Brayan Padrino <bdpadrino@gmail.com>
2017-06-16 22:13:04 -0400

    Reportes web Version1

---
+++ Vidly/Controllers/ReportController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report/Reporte
+        public ActionResult Reporte()
+        {
+
+            return View();
+        }
+
+        // GET: Report/DescargaPDF
+        public ActionResult DescargaPDF()
+        {
+
+
+
+
+            {
+                #region Datos dummy
+
+                List<Persona> datos = new List<Persona>(){
+                                            new Persona() { Nombre = "Sacarias", Apellido = "Piedras del Rio", Codigo = 1234 },
+                                            new Persona() { Nombre = "Alan", Apellido = "Brito", Codigo = 4323 },
+                                            new Persona() { Nombre = "Marcos", Apellido = "Pinto", Codigo = 9090 }
+            };
+
+                #endregion Datos dummy
+
+                string DirectorioReportesRelativo = "~/";
+                string urlArchivo = string.Format("{0}\\{1}.{2}", "Reportes", "MiReporte", "rdlc");
+
+                string FullPathReport = string.Format("{0}{1}",
+                                        this.HttpContext.Server.MapPath(DirectorioReportesRelativo),
+                                         urlArchivo);
+
+                ReportViewer Reporte = new ReportViewer();
+
+                Reporte.Reset();
+                Reporte.LocalReport.ReportPath = FullPathReport;
+                ReportDataSource DataSource = new ReportDataSource("DS_MiReporte", datos);
+                Reporte.LocalReport.DataSources.Add(DataSource);
+                Reporte.LocalReport.Refresh();
+                byte[] file = Reporte.LocalReport.Render("PDF");
+
+                return File(new MemoryStream(file).ToArray(),
+                          System.Net.Mime.MediaTypeNames.Application.Octet,
+                          /*Esto para forzar la descarga del archivo*/
+                          string.Format("{0}{1}", "archivoprueba.", "PDF"));
+            }
+
+        }
+    }
+}
+            /*#region Datos dummy
+
+            List<Persona> datos = new List<Persona>(){
+                new Persona() { Nombre = "Sacarias", Apellido = "Piedras del Rio", Codigo = 1234 },
+                new Persona() { Nombre = "Alan", Apellido = "Brito", Codigo = 4323 },
+                new Persona() { Nombre = "Marcos", Apellido = "Pinto", Codigo = 9090 }
+            };
+
+            #endregion Datos dummy
+
+            string DirectorioReportesRelativo = "~/";
+            //unir NOMBRE DEL ARCHIVO. EXTENSION
+            string urlArchivo = string.Format("{0}\\{1}.{2}", "Reportes","MiReporte", "rdlc");
+            
+            string FullPathReport = string.Format("{0}{1}",
+                  this.HttpContext.Server.MapPath(DirectorioReportesRelativo), urlArchivo);
+
+            ReportViewer Reporte = new ReportViewer();
+            
+            Reporte.Reset();
+            Reporte.LocalReport.ReportPath = FullPathReport;
+                                        //NOMBRE DEL DATA SOURCE Y LLENADO CON LISTA 
+            ReportDataSource DataSource = new ReportDataSource("DS_MiReporte", datos);
+            Reporte.LocalReport.DataSources.Add(DataSource);
+            // VERSION 2 PARAMETROS
+            // ReportParameter p = new ReportParameter("titulo", "Prueba");
+            //Reporte.LocalReport.SetParameters(p);
+            // VERSION 2
+            Reporte.LocalReport.Refresh();
+            byte[] file = Reporte.LocalReport.Render("PDF");
+
+            return File(new MemoryStream(file).ToArray(),
+                System.Net.Mime.MediaTypeNames.Application.Octet,
+                      //Esto para forzar la descarga del archivo
+                      string.Format("{0}{1}", "archivoprueba.", "PDF"));
+        }*/
+    
---
+++ Vidly/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Persona
+    {
+        public int Codigo { get; set; }
+
+        public String Nombre { get; set; }
+
+        public String Apellido { get; set; }
+
+    }
+}
